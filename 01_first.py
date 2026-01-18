bf3250e8600338d93f2718bd8471f7b0f6d2a86a
sumittomar1188-sketch <sumittomar1188@gmail.com>
2026-01-18 11:20:19 +0530

    added basic python programs

---
+++ 01_first.py
@@ -0,0 +1,8 @@
+print ("sumit")
+# now ham sab valur ek sath print karenge 
+print ("sumit",2,2.5,True)
+# ab iske output me jo space ara he usko change ese karenege
+print("sumit",2,2.5,True,sep = '/')
+# now ham ek hi line me do alag alag kese print 
+print ("sumit",end=" ") #yha mene space print karvaya he 
+print('baba')
---
+++ 02_datatype.py
@@ -0,0 +1,12 @@
+# here are the datatypes 
+#int
+print(2)
+print(1e308) # ye itne number of zero tak print kar sakta he 
+#float 
+print(2.50)
+print (1.5e308)# ye bi yha tak
+# boolean 
+print (True)
+print (False)
+#string 
+print ("sumit")
