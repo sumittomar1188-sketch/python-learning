96700edcf7a8252b909ca0937a1cebe8f68b2c02
sumittomar1188-sketch <sumittomar1188@gmail.com>
2026-01-19 08:42:16 +0530

    added python learning programs

---
+++ 04_variable.py
@@ -0,0 +1,26 @@
+# isme hamko do type ki writng hoti he ek c/c++ me use hoti he static or py me dynamic 
+# dynamic writting
+a= 5
+print(a)
+# matlab ki ham bina data type batae hi value aassing kar sakte he 
+# par c/c++ me esa name hota usme value ke data type ko batake hi value aasign karvani padti he
+# static writing
+# int a= 5 ese 
+
+#dynamic binding \
+b= 5
+print (b)
+b= "sumit"
+print (b)
+# isme ek hio variable me do bar value assign kar di he par c/c++ me nai ho sakta 
+# kyuki usme value fixed data type ki rehti he isme nahi rehti 
+
+
+
+# 1 interesting feature 
+a,b,c = 1,2,3
+print (a,b,c)
+
+
+a=b=c=5
+print (a,b,c)
---
+++ 05_user.py
@@ -0,0 +1,4 @@
+a = int(input("enter a number:-"))
+b= int (input("enetr another number:-"))
+print ("here is your answer:-",a+b)
+print (type(a),type(b))
