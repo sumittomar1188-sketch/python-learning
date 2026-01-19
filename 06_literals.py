42280c994fc50c7ef4aefbc4f6ab4143ec201d5e
sumittomar1188-sketch <sumittomar1188@gmail.com>
2026-01-19 19:27:43 +0530

    literals in basics of python

---
+++ 06_literals.py
@@ -0,0 +1,13 @@
+# jo value variable me assinge hoti he use letrals kehte he 
+a= 0b1010 # ye binary me sote ho rha he 
+b = 100 # ye simple hi hoA he 
+c = 0o301 # ye octal me hora he 
+d = 0x12c # ye hexa decimal me 
+print (a,b,c,d)
+
+
+# float literals 
+float_1 = 10.5
+float_2 = 1.5e3 # 1.5x10^3
+float_3 = 1.5e-3  #1.5x10^-3
+print(float_1,float_2,float_3)
