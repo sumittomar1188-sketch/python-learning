e0a6701f9e5cc83869c1133ea87de7273373c0b4
sumittomar1188-sketch <sumittomar1188@gmail.com>
2026-01-22 19:37:39 +0530

    basic nested loop programs

---
+++ 14_nested.py
@@ -0,0 +1,6 @@
+# isme loop ke andar loop chalega 
+# example ki jab hamko task diya jae ki ek school ke nadr jitni class he unke sabhi bacho ka name leke print karvao
+# tab ham pahle 1 to 12th tak ki class ke liye loop chalenge fir unke bachho ke liye
+for i in range(1,5):
+    for j in range (1,5):
+        print(i,j) # pahle ye loop me jaega tab i ki value 1 rahegi for j ke liye sabke pass jaega ese hi 4 bar chalega 
