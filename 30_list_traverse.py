f3798f56c08e15b3cdae22e047cbb0835f3dfbc6
sumittomar1188-sketch <sumittomar1188@gmail.com>
2026-02-03 22:21:52 +0530

    added some  baisic python programs

---
+++ 30_list_traverse.py
@@ -0,0 +1,14 @@
+# there are 2 ways of traverse in python
+# itemwise or stepwise
+
+# =========== item wise ===========
+l = [1,2,34,45,55]
+for i in l :
+    print(i)    
+
+# =========== stepwise ===========
+l1 = [1,2,3,4,5,6]
+for i in range (0,len(l1)):
+    # print(i) agar ese karenge to 0 se start karke print karega agar isko list me store karke karenge to 1 se dega 
+    print(l1[i])
+
